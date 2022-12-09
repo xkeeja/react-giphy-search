50c5004055b84f196661d2b990cd3d665945c27b
Roger Bian <113004083+xkeeja@users.noreply.github.com>
2022-12-09 10:59:35 +0900

    import & render App

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,10 @@
 import '../assets/stylesheets/application.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App';
+
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
